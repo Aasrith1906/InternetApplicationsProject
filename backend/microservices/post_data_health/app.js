b2d8c6abb359f2142728ad6a514130e849457b73
Aasrith Chennapragada <chennapragada.aasrith@gmail.com>
2022-11-26 19:01:23 +0000

    backend

--- backend/microservices/post_data_health/app.js
+++ backend/microservices/post_data_health/app.js
@@ -6,6 +6,11 @@ var s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
 AWS.config.update({ region: 'eu-west-2' });
 
+const {
+    randomUUID
+} = import('node:crypto');
+
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -26,14 +31,19 @@ AWS.config.update({ region: 'eu-west-2' });
 
 var S3BucketName = "hlsp-data-lake"
 
-function getS3Directory(input_data) {
+async function getS3Directory(input_data) {
+
+    const {
+        randomUUID
+    } = await import('node:crypto');
+
     var user = input_data.username
     var date = new Date()
     let year = date.getFullYear()
     let month = date.getMonth()
     let day = date.getDay()
 
-    var directory = `/${user}/${year}/${month}/${day}`
+    var directory = `healthData/${user}/${year}/${month}/${day}/${randomUUID()}`
 
     return directory
 
@@ -63,10 +73,10 @@ async function pushToS3(bucket_name, file_path, data) {
     var params = {
         Bucket: bucket_name,
         Key: file_path,
-        Body: Buffer.from(new Uint8Array(JSON.stringify(data)))
+        Body: JSON.stringify(data)
     }
 
-    output = await s3.putItem(params).promise()
+    output = await s3.upload(params).promise()
 }
 
 exports.lambdaHandler = async (event, context) => {
@@ -84,15 +94,15 @@ exports.lambdaHandler = async (event, context) => {
         event = JSON.parse(event.body)
         item_params = event
 
-        var continue_ = await checkUserExists(ddb, item_params.username.S)
+        var continue_ = await checkUserExists(ddb, item_params.username)
         if (continue_ == false) {
             response.statusCode = 404
             response.body = JSON.stringify({ "message": "user doesn't exist" })
             return response
         }
 
-        directory = getS3Directory(item_params)
-        output = pushToS3(S3BucketName, directory, item_params)
+        directory = await getS3Directory(item_params)
+        output = await pushToS3(S3BucketName, directory, item_params)
         response.body = JSON.stringify(output)
         return response
 
